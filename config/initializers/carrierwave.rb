3378d3154f311df2c1c50e739213aec0c24c9115
kichisuke <supernova39108@yahoo.co.jp>
2019-05-30 19:24:45 +0900

    Update carrierwave.rb
    画像保存時に、開発環境だとローカルに保存されるよう分岐。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,9 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?  
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -11,7 +14,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'mercari47w'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari47w'
+  end
 end
