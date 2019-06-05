8acf22fe1e0669a9c619818901f7080fdee790d7
yamamoto1108 <kjrps0826@gmail.com>
2019-06-05 21:10:56 +0900

    tab switching

---
+++ app/assets/javascripts/mypage.js
@@ -0,0 +1,19 @@
+$(function() {
+  $('.tab-news').click(function() {
+    $('.tab-news.tab-active').removeClass('tab-active');
+    $(this).addClass('tab-active');
+    $(".tab-news-content.tab-show").removeClass('tab-show');
+        // クリックしたタブからインデックス番号を取得
+    const index = $(this).index();
+        // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+    $(".tab-news-content").eq(index).addClass('tab-show');
+  });
+
+  $('.tab-buy').click(function() {
+    $('.tab-buy.tab-active').removeClass('tab-active');
+    $(this).addClass('tab-active');
+    $(".tab-buy-content.tab-show").removeClass('tab-show');
+    const index = $(this).index();
+    $(".tab-buy-content").eq(index).addClass('tab-show');
+  });
+});
