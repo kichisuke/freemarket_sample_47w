f9c68618143d50b693a5580b2068b089e6e89505
kichisuke <supernova39108@yahoo.co.jp>
2019-06-12 13:33:03 +0900

    add_profile_controller
    profileコントローラの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'test' => 'test#index'
   get 'test/mypage' => 'test#mypage', as: 'mypage'
+  get 'test/mypage/profile' => 'test#profile'
   root 'test#index'
 end
