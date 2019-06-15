a90796c3932d93545a1de00dfa2881377cb44c20
kichisuke <supernova39108@yahoo.co.jp>
2019-06-15 18:14:59 +0900

    edit deploy.rb 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.0"
 
 set :application, "freemarket_sample_47w"
-set :repo_url, "https://github.com/kichisuke/freemarket_sample_47w.git"
+set :repo_url, "git@github.com:kichisuke/freemarket_sample_47w.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
