7518bbe06c1c8502389a27e36d971dc68422bf1a
kichisuke <supernova39108@yahoo.co.jp>
2019-06-19 21:38:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -25,4 +25,25 @@ ActiveRecord::Schema.define(version: 20190619035129) do
     t.datetime "updated_at",           null: false
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                             default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "family_name",                                       null: false
+    t.string   "first_name"
+    t.string   "family_name_kana"
+    t.string   "first_name_kana"
+    t.string   "phone_number"
+    t.text     "profile_text",           limit: 65535
+    t.text     "avatar_image",           limit: 65535
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["nickname"], name: "index_users_on_nickname", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
 end
