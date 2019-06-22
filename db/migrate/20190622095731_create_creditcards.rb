b7ee19d8bcba1076af116165e435fb4b089a4050
kichisuke <supernova39108@yahoo.co.jp>
2019-06-22 21:31:25 +0900

    Update 20190622095731_create_creditcards.rb

--- db/migrate/20190622095731_create_creditcards.rb
+++ db/migrate/20190622095731_create_creditcards.rb
@@ -5,7 +5,7 @@ class CreateCreditcards < ActiveRecord::Migration[5.0]
       t.date :expiration_month,    null: false
       t.date :expiration_year,     null: false
       t.integer :security_code,    null: false
-      t.references :user,          null: false, foreign_key: true
+      # t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
   end
