73b5545aa74d2a98f96f8436f183e348094d0171
kichisuke <supernova39108@yahoo.co.jp>
2019-06-22 23:23:37 +0900

    modify_table_comments

--- db/migrate/20190622142314_create_comments.rb
+++ db/migrate/20190622142314_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :text,                null: false
-      # t.references :user,          null: false, foreign_key: true
+      t.references :user,          null: false, foreign_key: true
       t.references :item,          null: false, foreign_key: true
       t.timestamps
     end
