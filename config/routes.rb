7be283cf27a5e085abb4e2a44122ef136c8e5e27
kichisuke <supernova39108@yahoo.co.jp>
2019-06-22 20:12:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,21 +11,11 @@ Rails.application.routes.draw do
     get 'registration', to: 'users/registrations#registration'
   end
 
-  # get 'test', to: 'test#index'
-  # get 'test/mypage', to: 'test#mypage', as: 'mypage'
-  # get 'test/mypage/profile', to: 'test#profile'
-  # get 'test/mypage/card', to: 'test#card'
-  # get 'test/mypage/identification', to: 'test#user_identification'
-  # get 'test/mypage/logout', to: 'test#logout'
-  # get 'test/purchase', to: 'test#purchase', as: 'purchase'
-  # get 'signup', to: 'test#signup'
-  # get 'login', to: 'test#login'
-  # get 'registration', to: 'test#registration'
+  #下記は一旦残しておく
   get 'sms_confirmation', to: 'test#sms_confirmation'
   get 'input_address', to: 'test#input_address'
   get 'payment', to: 'test#payment'
   get 'signup_end', to: 'test#signup_end'
-  # root 'test#index'
   get 'test/sell', to: 'test#sell'
   get 'detail', to: 'test#detail'
 end
