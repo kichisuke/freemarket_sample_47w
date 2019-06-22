263ba8016fa03d17381f927188e8d4120a3a5cb7
kichisuke <supernova39108@yahoo.co.jp>
2019-06-22 23:16:22 +0900

    modify_table_sns_credentials

--- db/migrate/20190622141546_create_sns_credentials.rb
+++ db/migrate/20190622141546_create_sns_credentials.rb
@@ -2,7 +2,7 @@ class CreateSnsCredentials < ActiveRecord::Migration[5.0]
   def change
     create_table :sns_credentials do |t|
       t.string :provider,          unique:true
-      # t.references :user,          null: false, foreign_key:true
+      t.references :user,          null: false, foreign_key:true
       t.timestamps
     end
   end
