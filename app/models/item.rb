190d450d8f6fc199321f3cb4fa91e9d207886dc5
kichisuke <supernova39108@yahoo.co.jp>
2019-06-24 13:04:03 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand
-  has_many :images
+  has_many :item_images
   has_many :likes
   has_many :comments
   belongs_to :saler, class_name: "User"
