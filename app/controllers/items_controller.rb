f63b6c83d2d439be29b37425ecc4a6ae47c30fc7
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-06-25 19:18:41 +0900

    basic action check

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
-  
+
   def show
+    #@item = Item.find(params[:id])
+    @item = Item.find(1)
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,6 @@ class Item < ApplicationRecord
   has_many :images
   has_many :likes
   has_many :comments
-  belongs_to :saler, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :saler, class_name: "User", foreign_key: 'saler'
+  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id'
 end
