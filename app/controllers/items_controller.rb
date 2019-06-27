45446b3a6379373c21e22e0cd11094db76e11944
kichisuke <supernova39108@yahoo.co.jp>
2019-06-27 18:22:17 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   def index
     #レディースカテゴリーの4アイテムを最新の上から4つ抽出
-    @top_item1 = Item.where("category = '1'").order('created_at DESC').limit(4)
+    @top_item1 = Item.where("category_id = '1'").order('created_at DESC').limit(4)
     #メンズカテゴリーの4アイテムを最新の上から4つ抽出
-    @top_item2 = Item.where("category = '2'").order('created_at DESC').limit(4)
+    @top_item2 = Item.where("category_id = '2'").order('created_at DESC').limit(4)
   end
   
   def show
