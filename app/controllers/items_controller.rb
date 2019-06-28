38cab147617a9899638d4b9c8317c0ae96246586
masanori22 <aga.in@icloud.com>
2019-06-28 22:29:22 +0900

    form_for

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,14 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
   end
 end
