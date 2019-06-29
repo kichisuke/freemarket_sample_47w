fee49899f4b7ac805cf71378148460feb4d2f424
kichisuke <supernova39108@yahoo.co.jp>
2019-06-29 19:18:42 +0900

    add_test

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+describe Item do
+  describe '#index' do
+    it "gets 4 latest dates for specified category" do
+      item = Item.where("category_id = '1'").order('created_at DESC').limit(4)
+      expect(item).to be_truthy
+    end
+  end
+end
