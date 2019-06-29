503e31c1f13e463935937fbddd8ff3f0423ea1e5
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-06-29 19:10:59 +0900

    model fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,10 @@ class Item < ApplicationRecord
   has_many :images
   has_many :likes
   has_many :comments
-  belongs_to :saler, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :saler, class_name: "User", foreign_key: 'saler_id'
+  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id'
 
-  enum condition: { new: 0, near_unused: 1, low_damage: 2, mid_damage: 3, high_damage: 4, bad: 5 }
+  enum condition: { brand_new: 0, near_unused: 1, low_damage: 2, mid_damage: 3, high_damage: 4, bad: 5 }
   enum delivery_charge: { postage_include: 0, cash_on_delivery: 1 }
   enum delivery_method: { mercari: 0, yu_mail: 1, letter_pack: 2, normal_post: 3, kuroneko: 4, yu_pack: 5, click_post: 6, yu_packet: 7 }
   enum estimated_shipping_date: { shipping1: 0, shipping2: 1, shipping3: 2 }
