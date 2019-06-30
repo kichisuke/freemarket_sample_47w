217a14b02086a149d2c6fd195408ff64f0599ea7
yamamoto1108 <kjrps0826@gmail.com>
2019-06-30 16:14:33 +0900

    set action:done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -183,16 +183,25 @@ class ItemsController < ApplicationController
   end
 
   def purchase
-    @item = Item.find(params[:id])
     @user = User.find(1)
   end
 
   def pay
-    @item = Item.find(params[:id])
     @user = User.find(1)
     @item.buyer_id = @user.id
     @item.sales_status = 2
     @item.save
+    redirect_to :done
+  end
+
+  def done
+    @user = User.find(1)
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
