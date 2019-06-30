a1cc273e2cf6942739b78f75470bd982154e2060
yamamoto1108 <kjrps0826@gmail.com>
2019-06-30 14:40:38 +0900

    display user.address

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -118,4 +118,9 @@ class ItemsController < ApplicationController
   def move_to_login
     redirect_to controller: 'users/sessions', action: 'new' unless user_signed_in?
   end
+
+  def purchase
+    @item = Item.find(params[:id])
+    @user = User.find(1)
+  end
 end
