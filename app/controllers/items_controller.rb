f06153a50623ac3cc154df0f2f07169a5be79476
yamamoto1108 <kjrps0826@gmail.com>
2019-06-30 18:32:12 +0900

    install payjp

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -182,28 +182,6 @@ class ItemsController < ApplicationController
   def done
   end
 
-  def purchase
-    @user = User.find(1)
-  end
-
-  def pay
-    @user = User.find(1)
-    @item.buyer_id = @user.id
-    @item.sales_status = 2
-    @item.save
-    redirect_to :done
-  end
-
-  def done
-    @user = User.find(1)
-  end
-
-  private
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
   private
   def item_params
     params.require(:item).permit(:name, :price, :text, :category_id, :brand_id, :size, :condition, :delivery_charge, :delivery_method, :prefecture_id, :estimated_shipping_date, :sales_status).merge(saler_id: current_user.id)
