7e4f3f8d5f9e697bcfc7f79e9e83edc6b4328bfb
yamamoto1108 <kjrps0826@gmail.com>
2019-06-30 15:05:59 +0900

    set action:pay(except payjp)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -207,4 +207,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @user = User.find(1)
   end
+
+  def pay
+    @item = Item.find(params[:id])
+    @user = User.find(1)
+    @item.buyer_id = @user.id
+    @item.sales_status = 2
+    @item.save
+  end
 end
