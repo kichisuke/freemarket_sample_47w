0f6768169c8f4dc8e4f3a793265fb99ecdc5eec7
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-07-02 14:00:57 +0900

    previous, next item URL

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @otherItems = Item.where(saler_id: @item.saler.id).where.not(id: params[:id]).limit(3)
     @sameCategories = Item.where(category_id: @item.category.id).where.not(id: params[:id]).limit(3)
+    @previousItem = Item.where('id < ?', params[:id]).order('id DESC').first
+    @nextItem = Item.where('id > ?', params[:id]).order('id ASC').first
   end
 
   def new
