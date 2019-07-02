18588c271e199fe8e3ebc86b7edca1a6c78435ca
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-07-02 11:53:32 +0900

    edit controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    #@item = Item.find(params[:id])
-    @item = Item.find(1)
-    @images = ItemImage.where(item_id: 1)
-    @otherItems = Item.where(saler_id: 1).where.not(id: 1).limit(3)
+    @item = Item.find(params[:id])
+    @images = ItemImage.where(item_id: params[:id])
+    @otherItems = Item.where(saler_id: @item.saler.id).where.not(id: params[:id]).limit(3)
   end
 
   def new
