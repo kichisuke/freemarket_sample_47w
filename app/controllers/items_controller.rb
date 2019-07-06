3b91539fb29c228b8a759e493ca473808e07a3b8
yamamoto1108 <kjrps0826@gmail.com>
2019-07-06 21:16:27 +0900

    fix purchase button & redirect_to action: 'done'

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -128,7 +128,7 @@ class ItemsController < ApplicationController
     @item.buyer_id = @user.id
     @item.sales_status = 2
     @item.save
-    redirect_to :done
+    redirect_to action: 'done'
   end
 
   def done
