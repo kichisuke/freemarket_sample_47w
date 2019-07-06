a516d1857ba3a431bae5371cf460859db4f6fb38
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-06 17:08:29 +0900

    edit birthday column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190622142314) do
+ActiveRecord::Schema.define(version: 20190706053604) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postal_code",    default: "", null: false
@@ -103,8 +103,7 @@ ActiveRecord::Schema.define(version: 20190622142314) do
     t.string   "first_name",                     default: "", null: false
     t.string   "family_name_kana",               default: "", null: false
     t.string   "first_name_kana",                default: "", null: false
-    t.integer  "birth_year",                                  null: false
-    t.integer  "birth_month",                                 null: false
+    t.date     "birthday"
     t.text     "profile_text",     limit: 65535
     t.integer  "user_id",                                     null: false
     t.datetime "created_at",                                  null: false
