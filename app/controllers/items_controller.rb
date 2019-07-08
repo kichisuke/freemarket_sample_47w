15cca7fcc947f45e213129cfbcdb0e860024f1e1
kichisuke <supernova39108@yahoo.co.jp>
2019-07-08 12:54:23 +0900

    change_valiable_name
    変数名修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,9 @@ class ItemsController < ApplicationController
 
   def index
     #レディースカテゴリーの4アイテムを最新の上から4つ抽出
-    @top_item1 = Item.where("category_id = '1'").order('created_at DESC').limit(4)
+    @radiesItem = Item.where("category_id = '1'").order('created_at DESC').limit(4)
     #メンズカテゴリーの4アイテムを最新の上から4つ抽出
-    @top_item2 = Item.where("category_id = '2'").order('created_at DESC').limit(4)
+    @mensItem = Item.where("category_id = '2'").order('created_at DESC').limit(4)
   end
 
   def show
