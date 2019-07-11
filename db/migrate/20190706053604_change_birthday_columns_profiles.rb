938ce6d6715b2ac6c0803d51c473e357004d90c5
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-12 06:56:57 +0900

    edit migration file

--- db/migrate/20190706053604_change_birthday_columns_profiles.rb
+++ db/migrate/20190706053604_change_birthday_columns_profiles.rb
@@ -2,6 +2,6 @@ class ChangeBirthdayColumnsProfiles < ActiveRecord::Migration[5.0]
   def change
     remove_column :profiles, :birth_year, :integer
     remove_column :profiles, :birth_month, :integer
-    add_column :profiles, :birthday, :date, :after => :first_name_kana
+    add_column :profiles, :birthday, :date, :after => :first_name_kana, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20190706053604) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postal_code",    default: "", null: false
-    t.integer  "prefecture",                  null: false
+    t.integer  "prefecture_id",               null: false
     t.string   "city",           default: "", null: false
     t.string   "street_address", default: "", null: false
     t.string   "building"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 20190706053604) do
     t.integer  "condition",                                          null: false
     t.integer  "delivery_charge",                                    null: false
     t.integer  "delivery_method",                                    null: false
-    t.integer  "delivery_source_area",                               null: false
+    t.integer  "prefecture_id",                                      null: false
     t.integer  "estimated_shipping_date",                            null: false
     t.integer  "category_id"
     t.integer  "brand_id"
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 20190706053604) do
     t.string   "first_name",                     default: "", null: false
     t.string   "family_name_kana",               default: "", null: false
     t.string   "first_name_kana",                default: "", null: false
-    t.date     "birthday"
+    t.date     "birthday",                                    null: false
     t.text     "profile_text",     limit: 65535
     t.integer  "user_id",                                     null: false
     t.datetime "created_at",                                  null: false
