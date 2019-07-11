f73c799f147389e7db6b5ad49fd0036a98af8cd0
yamamoto1108 <kjrps0826@gmail.com>
2019-07-11 16:14:21 +0900

    set route for category_search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get :category_search
+    end
+  end
 
   devise_scope :user do
     get 'mypage', to: 'users/sessions#mypage'
