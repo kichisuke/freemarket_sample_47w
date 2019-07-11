e6d5da52d315ff0b3764344e038254dcf91c599c
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-12 06:53:16 +0900

    add validates address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  belongs_to :user
+
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\Z/ }
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :street_address, presence: true
 end
