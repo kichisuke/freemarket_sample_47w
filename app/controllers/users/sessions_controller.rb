3431dc27853d8cb15b486ca5c77245e11e7a4eb2
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-15 09:39:12 +0900

    delete not used devise controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
   def new
     super
   end
@@ -11,16 +9,4 @@ class Users::SessionsController < Devise::SessionsController
 
   def logout
   end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
