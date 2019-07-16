3bba63651a6e2ebc71555a283794d1aae733e89f
yamamoto1108 <kjrps0826@gmail.com>
2019-07-16 09:50:28 +0900

    search and show brand list

--- app/assets/javascripts/brand_search.js
+++ app/assets/javascripts/brand_search.js
@@ -1,4 +1,14 @@
 $(document).on('turbolinks:load', function() {
+  function appendBrand(brand) {
+    var html = `<li class="brand_search_result__list" data-brand-id="${brand.id}" data-brand-name="${brand.name}">
+                  ${brand.name}
+                </li>`
+    $('.brand_search_result').append(html);
+  }
+  function appendErrMsg(msg) {
+    var html = `<p class="brand_search_result__nothing">${msg}</p>`
+    $('.brand_search_result').append(html);
+  }
   $('#brand_search').on('keyup', function() {
     var input = $('#brand_search').val();
      $.ajax({
@@ -6,6 +16,18 @@ $(document).on('turbolinks:load', function() {
        type: 'GET',
        data: {keyword: input},
        dataType: 'json'
+     })
+
+     .done(function(brands) {
+       $('.brand_search_result').empty();
+       if (brands.length !== 0) {
+         brands.forEach(function(brand) {
+           appendBrand(brand);
+         })
+       }
+       else {
+        appendErrMsg("一致するブランドがありません");
+       }
      })
   });
 });
