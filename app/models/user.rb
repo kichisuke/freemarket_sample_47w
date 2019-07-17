d807ff21be8c14a79160ad720e29a3d4e2a83582
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-18 06:53:08 +0900

    add validates to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
 end
