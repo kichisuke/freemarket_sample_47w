105ba319790d2bc163b71e4e687130e26a946aaa
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-07-17 17:36:26 +0900

    do not accept input other than numbers

--- app/assets/javascripts/item-price.js
+++ app/assets/javascripts/item-price.js
@@ -1,14 +1,20 @@
-$(function(){
+$(function() {
   if(document.getElementById("item-price") != null) {
-    $("#item-price").on("keyup", function(){
-      var input = $("#item-price").val();
-      if (input >= 300 && input < 10000000) {
-        var commission = input / 10;
+    $('#item-price').on('input', function(e) {
+      let price = $(e.currentTarget).val();
+      price = price
+          .replace(/[０-９]/g, function(s) {
+              return String.fromCharCode(s.charCodeAt(0) - 65248);
+          })
+          .replace(/[^0-9]/g, '');
+      $(e.currentTarget).val(price);
+      if (price >= 300 && price < 10000000) {
+        var commission = price / 10;
         var commissionFloor = Math.floor(commission) ;
         $("#commission").empty();
         $("#commission").append("¥");
         $("#commission").append(commissionFloor.toLocaleString());
-        var profit = input - commissionFloor
+        var profit = price - commissionFloor
         $("#profit").empty();
         $("#profit").append("¥");
         $("#profit").append(profit.toLocaleString());
