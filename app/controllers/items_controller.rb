8e97a97153d1889fca5ed4068198109c624d9e3e
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-07-18 18:10:22 +0900

    fix conflict error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,37 +45,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
-  def item_params
-    params.require(:item).permit(:name, :price, :text, :category_id, :size, :condition, :delivery_charge, :delivery_method, :prefecture_id, :estimated_shipping_date, :sales_status, brand_attributes: [:id, :name]).merge(saler_id: current_user.id)
-  end
-
-  def new_image_params
-    params.require(:new_images).permit({images: []})
-
-  end
-
-  def create
-    @categories = Category.all
-    @item = Item.new(item_params)
-    if @item.save
-      params[:item_images][:image].each do |url|
-        @item.item_images.create(url: url, item_id: @item.id)
-      end
-      redirect_to root_path, notice: '出品しました。'
-    end
-  end
-
-  def category_search
-    respond_to do |format|
-      format.html
-      format.json do
-        @parent_category = Category.find(params[:parent_id])
-        @child_categories = @parent_category.children
-      end
-    end
-  end
-
   def brand_search
     @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
     respond_to do |format|
@@ -86,7 +55,11 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :price, :text, :category_id, :brand_id, :size, :condition, :delivery_charge, :delivery_method, :prefecture_id, :estimated_shipping_date, :sales_status, item_images_attributes: [:url])
+    params.require(:item).permit(:name, :price, :text, :category_id, :brand_id, :size, :condition, :delivery_charge, :delivery_method, :prefecture_id, :estimated_shipping_date, :sales_status, brand_attributes: [:id, :name], item_images_attributes: [:url])
+  end
+
+  def new_image_params
+    params.require(:new_images).permit({images: []})
   end
 
   def set_item
