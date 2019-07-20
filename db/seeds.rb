827277f33b2af5d6b1df404c6531cb58394b1f4e
kichisuke <supernova39108@yahoo.co.jp>
2019-07-20 19:44:40 +0900

    edit csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,38 @@
 require 'csv'
 
-CSV.foreach('db/users.csv', headers: true) do |row|
-  user = User.new(
-    id: row[0],
-    nickname: row[1],
-    email: row[2],
-    password: row[3],
-    avatar_image: row[4],
-    reset_password_token: row[5],
-    reset_password_sent_at: row[6],
-    remember_created_at: row[7],
-    created_at: row[8],
-    updated_at: row[9])
-  user.save!
-end
+# CSV.foreach('db/users.csv', headers: true) do |row|
+#   user = User.new(
+#     id: row[0],
+#     nickname: row[1],
+#     email: row[2],
+#     password: row[3],
+#     avatar_image: row[4],
+#     reset_password_token: row[5],
+#     reset_password_sent_at: row[6],
+#     remember_created_at: row[7],
+#     created_at: row[8],
+#     updated_at: row[9])
+#   user.save!
+# end
 
-CSV.foreach('db/brands.csv', headers: true) do |row|
-    brand = Brand.where(
-      id: row[0],
-      name: row[1],
-      created_at: row[2],
-      updated_at: row[3]).first_or_initialize
-    brand.save!
-end
+# CSV.foreach('db/brands.csv', headers: true) do |row|
+#     brand = Brand.where(
+#       id: row[0],
+#       name: row[1],
+#       created_at: row[2],
+#       updated_at: row[3]).first_or_initialize
+#     brand.save!
+# end
 
-CSV.foreach('db/categories.csv', headers: true) do |row|
-    category = Category.where(
-      id: row[0],
-      name: row[1],
-      parent_id: row[2],
-      created_at: row[3],
-      updated_at: row[4]).first_or_initialize
-    category.save!
-end
+# CSV.foreach('db/categories.csv', headers: true) do |row|
+#     category = Category.where(
+#       id: row[0],
+#       name: row[1],
+#       parent_id: row[2],
+#       created_at: row[3],
+#       updated_at: row[4]).first_or_initialize
+#     category.save!
+# end
 
 CSV.foreach('db/items.csv', headers: true) do |row|
     item = Item.where(
