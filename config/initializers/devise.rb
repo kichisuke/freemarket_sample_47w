e80ac05f01390a4ddf9733a6b6ca080207c33265
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-24 00:35:09 +0900

    add api keys :facebook and :google_oauth2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,4 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.secret_key ='secret_key'
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET']
 end
