9cfd05ddac121da905336fba9778ce75da7d07da
shibuya-akihiro <illusi0n.acts.like.magic14@gmail.com>
2019-07-24 00:32:23 +0900

    add columns to users
    Usersテーブルにuidカラムとproviderカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190714113606) do
+ActiveRecord::Schema.define(version: 20190722114458) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postal_code",    default: "", null: false
@@ -129,6 +129,8 @@ ActiveRecord::Schema.define(version: 20190714113606) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "uid"
+    t.string   "provider"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
