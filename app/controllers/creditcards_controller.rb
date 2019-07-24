5b074987214b5660dfb3b28799f03357658cca67
yamamoto1108 <kjrps0826@gmail.com>
2019-07-24 18:21:49 +0900

    set card_create

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -4,6 +4,19 @@ class CreditcardsController < ApplicationController
   end
 
   def create
+    Payjp.api_key = 'sk_test_5807e2b2840ba0fcf414ec61'
+    
+    if params['payjp-token'].blank?
+      redirect_to new_creditcard_path
+    else
+      customer = Payjp::Customer.create(description: 'test', card: params['payjp-token'])
+      @creditcard = Creditcard.new(user_id: current_user.id, customer_id: customer.id, token: params['payjp-token'], card_id: customer.default_card)
+      if @creditcard.save
+        redirect_to root_path
+      else
+        redirect_to new_creditcard_path
+      end
+    end
   end
 
 end
