3b46b3f927091751cff269d3405116a7d2f858d1
mizuho733 <48905598+mizuho733@users.noreply.github.com>
2019-08-15 16:23:29 +0900

    error fix

--- app/assets/javascripts/top-slider.js
+++ app/assets/javascripts/top-slider.js
@@ -6,7 +6,5 @@ $(document).on('turbolinks:load', function() {
     pauseOnDotsHover: true,
     infinite: true,
     arrows: true,
-    prevArrow: '<img src="/assets/carousel-prev.png" class="slick-prev">',
-    nextArrow: '<img src="/assets/carousel-next.png" class="slick-next">',
   });
 });
